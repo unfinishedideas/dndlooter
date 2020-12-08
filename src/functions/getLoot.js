b764bf6b84a530296296802b8ac526ea80dc3132
Peter Wells <petethebeatwells@gmail.com>
2020-12-07 23:04:28 -0800

    added type to determineTreasure to prevent confusing input problems

--- src/functions/getLoot.js
+++ src/functions/getLoot.js
@@ -129,32 +129,40 @@ const determineTreasure = (type, number, value) =>
     let result = {type: type, list: {}, totalNumber: number, totalValue: number*value};
     let table;
     
-    if (value === 10) {
-        table = lootTables.gems["10gp"];
-    } else if (value === 50) {
-        table = lootTables.gems["50gp"];
-    } else if (value === 100) {
-        table = lootTables.gems["100gp"];
-    } else if (value === 500) {
-        table = lootTables.gems["500gp"];
-    } else if (value === 1000) {
-        table = lootTables.gems["1000gp"];
-    } else if (value === 5000) {
-        table = lootTables.gems["5000gp"];
-    } else if (value === 25) {
-        table = lootTables.art_objects["25gp"];
-    } else if (value === 75) {
-        table = lootTables.art_objects["75gp"];
-    } else if (value === 250) {
-        table = lootTables.art_objects["250gp"];
-    } else if (value === 750) {
-        table = lootTables.art_objects["750gp"];
-    } else if (value === 2500) {
-        table = lootTables.art_objects["2500gp"];
-    } else if (value === 7500) {
-        table = lootTables.art_objects["7500gp"];
+    if (type === "gems") {
+        if (value === 10) {
+            table = lootTables.gems["10gp"];
+        } else if (value === 50) {
+            table = lootTables.gems["50gp"];
+        } else if (value === 100) {
+            table = lootTables.gems["100gp"];
+        } else if (value === 500) {
+            table = lootTables.gems["500gp"];
+        } else if (value === 1000) {
+            table = lootTables.gems["1000gp"];
+        } else if (value === 5000) {
+            table = lootTables.gems["5000gp"];
+        } else {
+            throw Error("determineTreasure: bad value for art_objects")
+        }
+    } else if (type === "art_objects") {
+        if (value === 25) {
+            table = lootTables.art_objects["25gp"];
+        } else if (value === 75) {
+            table = lootTables.art_objects["75gp"];
+        } else if (value === 250) {
+            table = lootTables.art_objects["250gp"];
+        } else if (value === 750) {
+            table = lootTables.art_objects["750gp"];
+        } else if (value === 2500) {
+            table = lootTables.art_objects["2500gp"];
+        } else if (value === 7500) {
+            table = lootTables.art_objects["7500gp"];
+        } else {
+            throw Error("determineTreasure: bad value for art_objects");
+        }
     } else {
-        return {list: "bad value input: determineTreasure", totalNumber: 0, totalValue: 0};
+        throw Error("determineTreasure: Bad type");
     }
 
     for (let i = 0; i < number; i++) {
