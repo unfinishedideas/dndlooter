a35512df99d64b21082d57d68bb72d45068ab9f2
Peter Wells <petethebeatwells@gmail.com>
2020-12-07 23:41:49 -0800

    replace long comparisons (d100) with func for faster writing

--- src/functions/getLoot.js
+++ src/functions/getLoot.js
@@ -10,46 +10,46 @@ export default function getLoot(lootInput)
     if (lootInput.type === 'individual') {
         // CR 0-4
         if (lootInput.cr === "0-4") {
-            if (d100_result >= 0 && d100_result <= 30) {
+            if (inRange(d100_result, 0, 30)) {
                 diceToRoll = lootTables.individualTreasure.CR["0-4"]["1-30"]
-            } else if (d100_result >= 31 && d100_result <= 60) {
+            } else if (inRange(d100_result, 31, 60)) {
                 diceToRoll = lootTables.individualTreasure.CR["0-4"]["31-60"]
-            } else if (d100_result >= 61 && d100_result <= 70) {
+            } else if (inRange(d100_result, 61, 70)) {
                 diceToRoll = lootTables.individualTreasure.CR["0-4"]["61-70"]
-            } else if (d100_result >= 71 && d100_result <= 95) {
+            } else if (inRange(d100_result, 71, 95)) {
                 diceToRoll = lootTables.individualTreasure.CR["0-4"]["71-95"]
             } else if (d100_result >= 96) {
                 diceToRoll = lootTables.individualTreasure.CR["0-4"]["96-100"]
             }
         // CR 5-10
         } else if (lootInput.cr === "5-10") {
-            if (d100_result >= 0 && d100_result <= 30) {
+            if (inRange(d100_result, 0, 30)) {
                 diceToRoll = lootTables.individualTreasure.CR["5-10"]["1-30"]
-            } else if (d100_result >= 31 && d100_result <= 60) {
+            } else if (inRange(d100_result, 31, 60)) {
                 diceToRoll = lootTables.individualTreasure.CR["5-10"]["31-60"]
-            } else if (d100_result >= 61 && d100_result <= 70) {
+            } else if (inRange(d100_result, 61, 70)) {
                 diceToRoll = lootTables.individualTreasure.CR["5-10"]["61-70"]
-            } else if (d100_result >= 71 && d100_result <= 95) {
+            } else if (inRange(d100_result,71, 95)) {
                 diceToRoll = lootTables.individualTreasure.CR["5-10"]["71-95"]
             } else if (d100_result >= 96){
                 diceToRoll = lootTables.individualTreasure.CR["5-10"]["96-100"]
             }
         // CR 11-16
         } else if (lootInput.cr === "11-16") {
-            if (d100_result >= 0 && d100_result <= 20) {
+            if (inRange(d100_result, 0, 20)) {
                 diceToRoll = lootTables.individualTreasure.CR["11-16"]["0-20"]
-            } else if (d100_result >= 21 && d100_result <= 35) {
+            } else if (inRange(d100_result, 21, 35)) {
                 diceToRoll = lootTables.individualTreasure.CR["11-16"]["21-35"]
-            } else if (d100_result >= 36 && d100_result <= 75) {
+            } else if (inRange(d100_result, 36, 75)) {
                 diceToRoll = lootTables.individualTreasure.CR["11-16"]["36-75"]
             } else if (d100_result >= 76) {
                 diceToRoll = lootTables.individualTreasure.CR["11-16"]["76-100"]
             }   
         // CR 17+
         } else if (lootInput.cr === "17+") {
-            if (d100_result >= 0 && d100_result <= 15) {
+            if (inRange(d100_result, 0, 15)) {
                 diceToRoll = lootTables.individualTreasure.CR["17+"]["0-15"]
-            } else if (d100_result >= 16 && d100_result <= 55) {
+            } else if (inRange(d100_result, 16, 55)) {
                 diceToRoll = lootTables.individualTreasure.CR["17+"]["16-55"]
             } else if (d100_result >= 56){
                 diceToRoll = lootTables.individualTreasure.CR["17+"]["56-100"]
@@ -63,68 +63,68 @@ export default function getLoot(lootInput)
         // CR 0-4
         if (lootInput.cr === "0-4") {
             diceToRoll = {cp: "6d6x100", sp: "3d6x100", gp: "2d6x10"}
-            if (d100_result >= 7 && d100_result <= 16) {
+            if (inRange(d100_result, 7, 16)) {
                 gems = determineTreasure(diceRoller(2,6).total, 10, 'gems');
-            } else if (d100_result >= 17 && d100_result <= 26) {
+            } else if (inRange(d100_result,17, 26)) {
                 artObjects = determineTreasure(diceRoller(2,4).total, 25, 'art_objects');
-            } else if (d100_result >= 27 && d100_result <= 36) {
+            } else if (inRange(d100_result, 27, 36)) {
                 gems = determineTreasure(diceRoller(2,6).total, 50, 'gems');
-            } else if (d100_result >= 37 && d100_result <= 44) {
+            } else if (inRange(d100_result, 37, 44)) {
                 gems = determineTreasure(diceRoller(2,6).total, 10, 'gems');
                 magicItems = determineMagicItems(
                     diceRoller(1,6).total, "A"
                 );
-            } else if (d100_result >= 45 && d100_result <= 52) {
+            } else if (inRange(d100_result, 45, 52)) {
                 artObjects = determineTreasure(diceRoller(2,4).total, 25, 'art_objects');
                 magicItems = determineMagicItems(
                     diceRoller(1,6).total, "A"
                 );
-            } else if (d100_result >= 53 && d100_result <= 60) {
+            } else if (inRange(d100_result, 53, 60)) {
                 gems = determineTreasure(diceRoller(2,6).total, 50, 'gems');
                 magicItems = determineMagicItems(
                     diceRoller(1,6).total, "A"
                 );
-            } else if (d100_result >= 61 && d100_result <= 65) {
+            } else if (inRange(d100_result, 61, 65)) {
                 gems = determineTreasure(diceRoller(2,6).total, 10, 'gems');
                 magicItems = determineMagicItems(
                     diceRoller(1,4).total, "B"
                 );
-            } else if (d100_result >= 66 && d100_result <= 70) {
+            } else if (inRange(d100_result, 66, 70)) {
                 artObjects = determineTreasure(diceRoller(2,4).total, 25, 'art_objects');
                 magicItems = determineMagicItems(
                     diceRoller(1,4).total, "B"
                 );
-            } else if (d100_result >= 71 && d100_result <= 75) {
+            } else if (inRange(d100_result, 71, 75)) {
                 gems = determineTreasure(diceRoller(2,6).total, 50, 'gems');
                 magicItems = determineMagicItems(
                     diceRoller(1,4).total, "B"
                 );
-            } else if (d100_result >= 76 && d100_result <= 78) {
+            } else if (inRange(d100_result, 76, 78)) {
                 gems = determineTreasure(diceRoller(2,6).total, 10, 'gems');
                 magicItems = determineMagicItems(
                     diceRoller(1,4).total, "C"
                 );
-            } else if (d100_result >= 79 && d100_result <= 80) {
+            } else if (inRange(d100_result, 79, 80)) {
                 artObjects = determineTreasure(diceRoller(2,4).total, 25, 'art_objects');
                 magicItems = determineMagicItems(
                     diceRoller(1,4).total, "C"
                 );
-            } else if (d100_result >= 81 && d100_result <= 85) {
+            } else if (inRange(d100_result, 81, 85)) {
                 gems = determineTreasure(diceRoller(2,6).total, 50, 'gems');
                 magicItems = determineMagicItems(
                     diceRoller(1,4).total, "C"
                 );
-            } else if (d100_result >= 86 && d100_result <= 92) {
+            } else if (inRange(d100_result, 86, 92)) {
                 artObjects = determineTreasure(diceRoller(2,4).total, 25, 'art_objects');
                 magicItems = determineMagicItems(
                     diceRoller(1,4).total, "F"
                 );
-            } else if (d100_result >= 93 && d100_result <= 97) {
+            } else if (inRange(d100_result, 93, 97)) {
                 gems = determineTreasure(diceRoller(2,6).total, 50, 'gems');
                 magicItems = determineMagicItems(
                     diceRoller(1,4).total, "F"
                 );
-            } else if (d100_result >= 98 && d100_result <= 99) {
+            } else if (inRange(d100_result, 98, 99)) {
                 artObjects = determineTreasure(diceRoller(2,4).total, 25, 'art_objects');
                 magicItems = determineMagicItems(1, "G");
             } else if (d100_result === 100) {
@@ -158,6 +158,15 @@ export default function getLoot(lootInput)
     }
 
     return totals;
+}
+
+const inRange = (testNumber, lowNumber, highNumber) => 
+{
+    if (testNumber >= lowNumber && testNumber <= highNumber) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
 const determineTreasure = (number, value, type) =>
