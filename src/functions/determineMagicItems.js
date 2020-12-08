585d68d652695227abf14200013a3e921fcc05d1
Peter Wells <petethebeatwells@gmail.com>
2020-12-08 00:39:03 -0800

    confirmed that inRange works on determineMagicItems & set up d100 roller

--- src/functions/determineMagicItems.js
+++ src/functions/determineMagicItems.js
@@ -1,8 +1,24 @@
 import * as lootTables from '../tables/lootTable';
-import inRange from './getLoot';
+
+// functions
+import { inRange } from './getLoot';
+import diceRoller from './diceRoller';
 
 export default function determineMagicItems(timesToRollOnTable, tableLetter)
 {
-    console.log('still gotta write this determine magic items biz')
+    const d100_results = [];
+    for (let i = 0; i< timesToRollOnTable; i++) {
+        d100_results.push(diceRoller(1,100).total);
+    }
+
+    d100_results.forEach((result) => {
+        console.log('rolled a ', result);
+        if (inRange(result, 0, 50)) {
+            console.log("1-50");
+        } else if (inRange(result, 51, 100)) {
+            console.log("51-100");
+        }
+    })
+
     return 'hi 3';
 }
